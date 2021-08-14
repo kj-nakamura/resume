b9809bac2a1978791ae29b46807201e0219e5f3f
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2021-08-14 17:52:42 +0900

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
-use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         if (env('APP_ENV') === 'production') {
-            \URL::forceScheme('https');
+            $url->forceScheme('https');
         }
     }
 }
